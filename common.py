cea88eef0ed6d9e8c0cdf3ce03c4130286487bfa
stevecastledine@gmail.com <stevecastledine@gmail.com>
2022-04-12 08:31:25 +0000

    55

--- common.py
+++ common.py
@@ -1,5 +1,11 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
 def commonfunction(name):
   return "hello "+name
 
 def newfunction(name):
     return name
+
+def processDF(df):
+    return df.withColumn("silver", lit("Hello Silver World")).withColumn("repos", lit("Hello repos"))
